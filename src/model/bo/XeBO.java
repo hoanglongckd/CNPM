b12af9b4f4df1a82e37822adc869c8b9ac05b0bf
Nguyen Nam <nguyennam.271@gmail.com>
2016-10-24 00:16:36 +0700

    a little bit

--- src/model/bo/XeBO.java
+++ src/model/bo/XeBO.java
@@ -4,52 +4,52 @@ import java.util.ArrayList;
 
 import model.bean.TaiXeBEAN;
 import model.bean.XeBEAN;
-import model.dao.XeDao;
+import model.dao.XeDAO;
 
 public class XeBO {
 
-	static XeDao XeDao = new XeDao();
+	static XeDAO xeDAO = new XeDAO();
 	
 	
 	public static ArrayList<XeBEAN> lietKeListXe() {
 		
-		return XeDao.lietKeListXe();
+		return xeDAO.lietKeListXe();
 	}
 
 
 	public static boolean xoaXe(int id) {
-		return XeDao.xoaXe(id);
+		return xeDAO.xoaXe(id);
 	}
 
 
 	public static XeBEAN getCapNhatXe(int id) {
-		return XeDao.getCapNhatXe(id);
+		return xeDAO.getCapNhatXe(id);
 	}
 
 
 	public static boolean setCapNhatXe(XeBEAN xe) {
-		return XeDao.setCapNhatXe(xe);
+		return xeDAO.setCapNhatXe(xe);
 	}
 
 
 	public static boolean setCapNhatPhanCongTX(TaiXeBEAN taixe, XeBEAN xe) {
 		
-		return XeDao.setCapNhatPhanCongTX(taixe, xe);
+		return xeDAO.setCapNhatPhanCongTX(taixe, xe);
 	}
 
 
 	public static boolean themXe(XeBEAN xe) {
-		return XeDao.themXe(xe);
+		return xeDAO.themXe(xe);
 	}
 
 
 	public static boolean themPhanCongTX(TaiXeBEAN taixe, XeBEAN xe) {
 		
-		return XeDao.themPhanCongTX(taixe, xe);
+		return xeDAO.themPhanCongTX(taixe, xe);
 	}
 
 	public static int getMaxXeId() {
-		return XeDao.getMaxXeId();
+		return xeDAO.getMaxXeId();
 	}
 	
 
