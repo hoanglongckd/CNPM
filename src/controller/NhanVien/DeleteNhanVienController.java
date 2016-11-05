4bea31ea09b0652a061663127789c728e8cd9519
Long <hoanglongckd@gmail.com>
2016-11-05 14:12:43 +0700

    Update font xóa nhân viên controller

--- src/controller/NhanVien/DeleteNhanVienController.java
+++ src/controller/NhanVien/DeleteNhanVienController.java
@@ -1,9 +1,7 @@
 package controller.NhanVien;
 
 import java.io.IOException;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,32 +9,18 @@ import javax.servlet.http.HttpSession;
 
 import model.bo.NhanVienBO;
 
-/**
- * Servlet implementation class DeleteNhanVienController
- */
-//@WebServlet("/DeleteNhanVienController")
 public class DeleteNhanVienController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public DeleteNhanVienController() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doPost(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession msg = request.getSession();
 		
@@ -45,11 +29,11 @@ public class DeleteNhanVienController extends HttpServlet {
 		String idNhanVien= request.getParameter("idNhanVien");
 		
 		if(nhanVienBO.isNhanVienDeleted(idNhanVien)){
-			msg.setAttribute("messages", "<ul><li>X骯 nh鈔 vi阯 th鄋h c鬾g!</li></ul>");
+			msg.setAttribute("messages", "<ul><li>X贸a nh芒n vi锚n th脿nh c么ng!</li></ul>");
 			response.sendRedirect(request.getContextPath() + "/danh-sach-nhan-vien");
 		}
 		else{
-			msg.setAttribute("errors", "<ul><li>X骯 nh鈔 vi阯 kh鬾g th鄋h c鬾g</li></ul>");	
+			msg.setAttribute("errors", "<ul><li>X贸a nh芒n vi锚n kh么ng th脿nh c么ng!</li></ul>");	
 			response.sendRedirect(request.getContextPath() + "/danh-sach-nhan-vien");
 		}
 		
