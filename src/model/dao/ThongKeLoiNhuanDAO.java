727c768073522ba7b50361024182e71e0f8871aa
Long <hoanglongckd@gmail.com>
2016-11-22 10:36:14 +0700

    Fix thong ke theo nam

--- src/model/dao/ThongKeLoiNhuanDAO.java
+++ src/model/dao/ThongKeLoiNhuanDAO.java
@@ -48,23 +48,41 @@ public class ThongKeLoiNhuanDAO {
 	public ArrayList<ThongKeLoiNhuanBEAN> thongKeTheoNam (String year) {
 		ArrayList<ThongKeLoiNhuanBEAN> list = new ArrayList<>();
 		try {
-			String sql = "SELECT XE.BienSoXe, SUM(GIATIEN.ThanhTien) as Money, SUM(BAODUONGXE.SoTien) as Cost "
-					+ "FROM GIATIEN "
-					+ "FULL JOIN DIEUPHOI ON GIATIEN.idDieuPhoi = DIEUPHOI.id "
-					+ "FULL JOIN PHANCONGTX ON DIEUPHOI.idPhanCong = PHANCONGTX.id "
-					+ "FULL JOIN XE ON XE.id = PHANCONGTX.idXe "
-					+ "FULL JOIN BAODUONGXE ON XE.id = BAODUONGXE.idXe "
-					+ "WHERE YEAR(DIEUPHOI.ThoiGianBatDau) = ? OR YEAR(BAODUONGXE.NgayBaoDuong) = ? GROUP BY XE.BienSoXe";
+			String sql = "SELECT * FROM "
+						+ "(SELECT XE.BienSoXe as Xe1, SUM(GIATIEN.ThanhTien) as Cost "
+						+ "FROM XE JOIN PHANCONGTX "
+						+ "ON XE.id = PHANCONGTX.idXe "
+						+ "JOIN DIEUPHOI "
+						+ "ON PHANCONGTX.id = DIEUPHOI.idPhanCong "
+						+ "JOIN GIATIEN "
+						+ "ON DIEUPHOI.id = GIATIEN.idDieuPhoi "
+						+ "WHERE YEAR(DIEUPHOI.ThoiGianKetThuc) = ? "
+						+ "GROUP BY Xe.BienSoXe) as i "
+						+ "FULL JOIN "
+						+ "(SELECT XE.BienSoXe as Xe2, SUM(BAODUONGXE.SoTien) as Fee "
+						+ "FROM XE JOIN BAODUONGXE "
+						+ "ON XE.id = BAODUONGXE.idXe "
+						+ "WHERE YEAR(BAODUONGXE.NgayBaoDuong) = ? "
+						+ "GROUP BY XE.BienSoXe) as j "
+						+ "ON i.Xe1 = j.Xe2";
 			PreparedStatement ps = conn.prepareStatement(sql);
 			ps.setString(1, year);
 			ps.setString(2, year);
 			rs = ps.executeQuery();
 			while (rs.next()) {
-				ThongKeLoiNhuanBEAN item = new ThongKeLoiNhuanBEAN(
-						rs.getString("BienSoXe"), 
-						rs.getLong("Money"), 
-						rs.getLong("Cost"));
-				list.add(item);
+				if (rs.getString("Xe1") != null) {
+					ThongKeLoiNhuanBEAN item = new ThongKeLoiNhuanBEAN(
+							rs.getString("Xe1"), 
+							rs.getLong("Fee"), 
+							rs.getLong("Cost"));
+					list.add(item);
+				} else {
+					ThongKeLoiNhuanBEAN item = new ThongKeLoiNhuanBEAN(
+							rs.getString("Xe2"), 
+							rs.getLong("Fee"), 
+							rs.getLong("Cost"));
+					list.add(item);
+				}
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
